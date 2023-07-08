15af2d55ff0f3f503c35a9c3c82a610f79154249
Uygar Muntaş <muntasuygar@gmail.com>
2023-07-09 00:43:03 +0300

    stack implement in python

--- main.py
+++ main.py
@@ -32,6 +32,48 @@ import math
 # d = (b*b) - 4*a*c
 
 # if d >= 0 :
-#     r1 = (-b+)
+#     r1 = (-b+ math.sqrt(d)) /2
+#     r2 = (-b - math.sqrt(d)) /2
+# else :  
+#     rp = -b/2*a
+#     ip = math.sqrt((-d)/2*a) # i didnt understand this math section
+#     print(rp + ip)
 
-print(math.sqrt(121))
+
+#############################################
+# Algorithm 4
+#############################################
+# Algorithm 5
+#############################################
+# Algorithm 6
+
+##########################################################################################
+# stack implementation in python
+
+# creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+# creating an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+# adding items into the stack   
+def push(stack,item):
+    stack.append(item)
+    print("pushed item : " + item)
+
+# removing an element from the stack
+def pop(stack):
+    if(check_empty(stack)):
+        return "stack is empty"
+    return stack.pop()
+
+stack = create_stack()
+push(stack,str(1))
+push(stack,str(2))
+push(stack,str(3))
+push(stack,str(4))
+print("popped item " + pop(stack))
+print("stack after popping an element: " + str(stack))
