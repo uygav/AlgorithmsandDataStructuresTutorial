db9c03e83b2f26e8dedb4eba7cf49f5339ca97c1
Uygar Muntaş <muntasuygar@gmail.com>
2023-07-09 01:21:56 +0300

    queue implementation in python

--- main.py
+++ main.py
@@ -51,29 +51,67 @@ import math
 # stack implementation in python
 
 # creating a stack
-def create_stack():
-    stack = []
-    return stack
+# def create_stack():
+#     stack = []
+#     return stack
 
-# creating an empty stack
-def check_empty(stack):
-    return len(stack) == 0
+# # creating an empty stack
+# def check_empty(stack):
+#     return len(stack) == 0
 
-# adding items into the stack   
-def push(stack,item):
-    stack.append(item)
-    print("pushed item : " + item)
+# # adding items into the stack   
+# def push(stack,item):
+#     stack.append(item)
+#     print("pushed item : " + item)
 
-# removing an element from the stack
-def pop(stack):
-    if(check_empty(stack)):
-        return "stack is empty"
-    return stack.pop()
+# # removing an element from the stack
+# def pop(stack):
+#     if(check_empty(stack)):
+#         return "stack is empty"
+#     return stack.pop()
 
-stack = create_stack()
-push(stack,str(1))
-push(stack,str(2))
-push(stack,str(3))
-push(stack,str(4))
-print("popped item " + pop(stack))
-print("stack after popping an element: " + str(stack))
+# stack = create_stack()
+# push(stack,str(1))
+# push(stack,str(2))
+# push(stack,str(3))
+# push(stack,str(4))
+# print("popped item " + pop(stack))
+# print("stack after popping an element: " + str(stack))
+
+
+##########################################################################################
+# queue implementation in python
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    # add an element
+    def enqueue(self,item):
+        self.queue.append(item)
+
+    # remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+    
+    # display the queue
+    def display(self):
+        print(self.queue)
+    def size(self):
+        return(len(self.queue))
+    
+q = Queue()
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+q.display()
+
+q.dequeue()
+
+print("after removing an element")
+q.display()
