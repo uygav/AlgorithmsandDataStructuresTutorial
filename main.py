3e46e2b4d031f8086e25e796e14cda8ee322e823
Uygar Muntaş <muntasuygar@gmail.com>
2023-07-08 23:55:53 +0300

    algorithm tutorial exercises

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-print("uygar")
-print(31)
+# https://www.programiz.com/dsa/algorithm 
+# Algorithm 1
+# num1 = int(input("declare number 1 :"))
+# num2 = int(input("declare number 2 :"))
+
+# sum = num1 + num2   
+# print("sum is " + str(sum))
+
+#############################################
+# Algorithm 2
+# a = int(input("declare a number 1 :"))
+# b = int(input("declare b number 2 :"))
+# c = int(input("declare c number 3 :"))
+
+# if a>b :
+#     if a >c:
+#         print("a is the largest number")
+#     else :
+#         print(" c is the largest number")
+# else :
+#     if b >c :
+#         print("b is the largest number")
+#     else :
+#         print("c is the greatest number") 
+
+#############################################
+# Algorithm 3
+import math
+# a = input("declare a number :")
+# b = input("declare b number :")
+# c = input("declare c number :")
+# d = (b*b) - 4*a*c
+
+# if d >= 0 :
+#     r1 = (-b+)
+
+print(math.sqrt(121))
